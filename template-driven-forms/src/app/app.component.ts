159c924a43b7c06d5194593cad64894fbc5092a4
Muniox <muniox20@gmail.com>
2024-05-23 20:53:29 +0200

    TD: Handling Radio Buttons

--- template-driven-forms/src/app/app.component.ts
+++ template-driven-forms/src/app/app.component.ts
@@ -10,7 +10,8 @@ export class AppComponent {
   // @ViewChild(NgForm, { static: true }) signupForm: NgForm;
   @ViewChild('f', { static: true }) signupForm: NgForm; //static tylko wtedy jak nie ma structural directive np. ngIf
   defaultQuestion = 'pet';
-  answer: string = ''
+  answer: string = '';
+  genders = ['male', 'female'];
 
   suggestUserName() {
     const suggestedName = 'Superuser';
