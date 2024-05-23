c57cf39debe43b814059d495ccb3ee48a0ed2711
Muniox <muniox20@gmail.com>
2024-05-23 19:43:47 +0200

    TD: Accessing the form with @ViewChild

--- template-driven-forms/src/app/app.component.ts
+++ template-driven-forms/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import {NgForm} from "@angular/forms";
 
 @Component({
@@ -7,11 +7,18 @@ import {NgForm} from "@angular/forms";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  // @ViewChild(NgForm, { static: true }) signupForm: NgForm;
+  @ViewChild('f', { static: true }) signupForm: NgForm; //static tylko wtedy jak nie ma structural directive np. ngIf
+
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
 
-  onSubmit(form: NgForm) {
-    console.log(form)
+  // onSubmit(form: NgForm) {
+  //   console.log(form)
+  // }
+
+  onSubmit() {
+    console.log(this.signupForm)
   }
 }
